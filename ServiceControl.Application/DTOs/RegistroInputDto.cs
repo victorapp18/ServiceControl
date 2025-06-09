77311d8cd8faaa696ca590f7f5a4a161aaf407dc
Victor Arthur de Moraes da Cunha <victor.cunha@montreal.com.br>
2025-06-08 23:38:07 -0300

    ajuste no id

--- ServiceControl.Application/DTOs/RegistroInputDto.cs
+++ ServiceControl.Application/DTOs/RegistroInputDto.cs
@@ -2,7 +2,6 @@
 {
     public class RegistroInputDto
     {
-        public string Id { get; set; } = string.Empty;
         public string ServicoExecutado { get; set; } = string.Empty;
         public DateTime Data { get; set; }
         public string Responsavel { get; set; } = string.Empty;
